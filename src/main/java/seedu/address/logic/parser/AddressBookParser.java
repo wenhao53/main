47e284e5640dc069ac8a685f92cf2d7e894ed9b2
wenhao53 <wenhao53@gmail.com>
2018-04-06 01:22:37 +0800

    Added ClassificationParser and edited AddressBookParser to include classification command

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -9,6 +9,7 @@ import java.util.regex.Pattern;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.CalendarCommand;
 import seedu.address.logic.commands.CaloriesCommand;
+import seedu.address.logic.commands.ClassificationCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteCommand;
@@ -48,6 +49,7 @@ public class AddressBookParser {
 
         final String commandWord = matcher.group("commandWord");
         final String arguments = matcher.group("arguments");
+
         switch (commandWord) {
         /**
         * Adapted from https://nus-cs2103-ay1718s2.github.io/
@@ -76,6 +78,9 @@ public class AddressBookParser {
         case FindCommand.COMMAND_WORD:
         case FindCommand.COMMAND_ALIAS:
             return new FindCommandParser().parse(arguments);
+
+        case ClassificationCommand.COMMAND_WORD:
+            return new ClassificationCommandParser().parse(arguments);
 
         case CaloriesCommand.COMMAND_WORD:
         case CaloriesCommand.COMMAND_ALIAS:
