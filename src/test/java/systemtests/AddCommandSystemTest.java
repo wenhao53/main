5a10576ab8719d9170214efc7cf6514465c71b8b
hypertun <hypertun@live.com>
2018-04-11 02:46:12 +0800

    checkstyle 3

--- src/test/java/systemtests/AddCommandSystemTest.java
+++ src/test/java/systemtests/AddCommandSystemTest.java
@@ -211,7 +211,8 @@ public class AddCommandSystemTest extends AddressBookSystemTest {
                 + TAG_DESC_FRIEND;
         assertCommandSuccess(command, toAdd);
 
-        /* Case: add a person with all fields same as another person in the address book except activityLevel -> added */
+        /* Case: add a person with all fields same as another person in the address book except activityLevel ->
+        added */
         toAdd = new PersonBuilder().withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY).withEmail(VALID_EMAIL_AMY)
                 .withAddress(VALID_ADDRESS_AMY).withHeight(VALID_HEIGHT_AMY).withWeight(VALID_WEIGHT_AMY)
                 .withGender(VALID_GENDER_AMY)
